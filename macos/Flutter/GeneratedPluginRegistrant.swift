a66b67505a7463c7e5c8f249d94ad6d7c3fb4f20
Aravind M S <aravindmangattu38@gmail.com>
2023-02-19 19:17:45 +0530

    bug fix 1

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,8 +9,8 @@ import acr_cloud_sdk
 import audio_service
 import audio_session
 import just_audio
-import path_provider_macos
-import shared_preferences_macos
+import path_provider_foundation
+import shared_preferences_foundation
 import sqflite
 import url_launcher_macos
 
